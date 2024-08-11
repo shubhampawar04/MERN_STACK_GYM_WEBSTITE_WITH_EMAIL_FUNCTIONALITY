a69cf2b5d71d5d1c4b119578bae6835dd3335c62
shubhampawar04 <pawarshubham81668@gmail.com>
2024-08-11 08:25:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import {BrowserRouter as router} from 'react-router-dom'
+
+const App = () => {
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
